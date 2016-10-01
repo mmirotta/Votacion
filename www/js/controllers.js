7ef302378bdcc9c5b09b61cb8dd93baaeb6f6a45
mmirotta <m.mirotta@gmail.com>
2016-10-01 11:43:47 -0300

    verifica email y registra usuarios.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -13,7 +13,8 @@ angular.module('starter.controllers', [])
 })
 
 .controller('LoginCtrl', function($scope, $stateParams, $timeout) {
-  $scope.logueado = false;
+  $scope.logueado = 'no';
+  $scope.verificado = 'no';
 
   $scope.login = {};
   $scope.login.usuario = "m.mirotta@gmail.com";
@@ -21,15 +22,70 @@ angular.module('starter.controllers', [])
 
   $scope.Logear = function (){
     firebase.auth().signInWithEmailAndPassword($scope.login.usuario, $scope.login.clave).catch(function (error){
-      console.info("incorrecto", error);
+
+      console.info("Error", error);
     }).then( function(resultado){
-      $timeout(function() {$scope.logueado = true}, 1);
-      console.info("correcto", resultado);
+
+      $timeout(function() {
+        $scope.logueado = 'si';
+        if (resultado.emailVerified == false)
+          $scope.verificado = 'si';
+        else
+          $scope.verificado = 'no';
+
+      });
+      console.info("login correcto", resultado);
+
     });
-  }
+  };
 
   $scope.Deslogear = function (){
-    $timeout(function() {$scope.logueado = false}, 1);
-  }
+    firebase.auth().signOut().catch(function (error){
+      console.info("login incorrecto", error);
+    }).then( function(resultado){
+      $timeout(function() {
+        $scope.logueado = 'no';
+        $scope.usuario = JSON.stringify(resultado);
+        //$scope.usuario = JSON.stringify(firebase.auth().currentUser()); Esto es para usarlo en cualquier lado porque firebase esta global
+      });
+      console.info("deslogueo correcto", resultado);
+    });
+  };
+
+  $scope.Resetear = function (){
+    firebase.auth().sendPasswordResetEmail($scope.login.usuario).then(function(resultado){
+      console.info("resertear clave correcto", resultado);
+    }).catch(function (error){
+      console.info("resertear clave incorrecto", error);
+    });
+  };
+
+  $scope.VerificarMail = function (){
+    firebase.auth().currentUser.sendEmailVerification().then(function(resultado){
+      console.info("verifico el usuario correcto", resultado);
+    }).catch(function (error){
+      console.info("verifico el usuario incorrecto", error);
+    });
+  };
+
+  $scope.Registrar = function (){
+    firebase.auth().createUserWithEmailAndPassword($scope.login.usuario, $scope.login.clave).then(function(resultado){
+      $timeout(function() {
+        $scope.logueado = 'si';
+        if (resultado.emailVerified == false)
+          $scope.verificado = 'si';
+        else
+          $scope.verificado = 'no';
+
+      });
+      console.info("registrar el usuario correcto", resultado);
+    },function (error){
+      if(error.code == "auth/email-already-in-use")
+        $scope.errorMensaje = "El email ya esta registrado";
+
+      console.info("registrar el usuario incorrecto", error);
+    });
+  };
+
 });
 
